3ba85bbd6ef5822e9d69b2860290094fea605ee9
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-27 19:34:15 -0500

    Initial component App

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react'
+
+import Card from '@/components/Card'
+import Game from '@/components/Game'
+import useGames from '@/store/useGames'
+
+function App () {
+  const { games, actualGame, selectGame } = useGames()
+  const clientTMI = useGames(p => p.client)
+
+  useEffect(() => {
+    if (!clientTMI) return
+
+    clientTMI.connect()
+
+    return () => {
+      clientTMI.disconnect()
+    }
+  }, [clientTMI])
+
+  const handleSelectGame = e => {
+    selectGame(e)
+  }
+
+  if (actualGame) {
+    return <Game {...actualGame} score={{ high: 20, player: 'jp__is' }} />
+  }
+
+  return (
+    <div className='grid gap-3 grid-repeat-130 max-w-2xl m-auto'>
+      {games.map(game => (
+        <Card
+          key={game.id}
+          className='gap-1 flex-col cursor-default'
+          onClick={() => handleSelectGame(game.id)}
+        >
+          {game.icon && <span className='text-2xl'>{game.icon}</span>}
+          <h3>{game.title}</h3>
+        </Card>
+      ))}
+    </div>
+  )
+}
+
+export default App
