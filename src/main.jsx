937b0254b54065098332fcb9524f1224f4e9141d
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-27 19:33:31 -0500

    Main componente React

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import './index.css'
+import 'virtual:uno.css'
+import '@unocss/reset/tailwind-compat.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
