ccf58f82c69b38555dd2d6295636275baeb0de1b
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-29 16:39:54 -0500

    Create Button component using ui.ibelick

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import { twMerge } from 'tailwind-merge'
+
+const variants = {
+  primary:
+    'inline-flex text-lg py-2 animate-background-shine items-center justify-center rounded-md border border-white bg-image-[linear-gradient(110deg,#000103,45%,#9146FF,55%,#000103)] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'
+}
+
+const Button = ({ children, className = '', variant = 'primary' }) => {
+  const _class = twMerge(variants[variant] ?? variants.primary, className)
+  return <button className={_class}>{children}</button>
+}
+
+export default Button
