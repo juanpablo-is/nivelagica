4474dcffdd1acd79d58337f1c02ee5d109bbf8d9
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-29 17:45:20 -0500

    Create handler to auth in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,29 @@
+import { authURL } from '@/api/Twitch'
+
 import Button from '@/components/Button'
 import Footer from '@/components/Footer'
 
 const Home = () => {
+  function handlerAuth () {
+    window.location.href = authURL()
+  }
+
   return (
     <div className='flex flex-col w-full h-screen items-center p-3 gap-2 font-primary'>
       <main className='flex-1 flex flex-col justify-center gap-3 items-center'>
         <header className='flex flex-col gap-1 items-center'>
           <span className='fs-calc(0.4em_+_3.5vw)'>Juega</span>
           <h1 className='fs-calc(0.5em_+_8vw)'>
-            {[...'NUMÉRICA'].map(letter => (
-              <span className='h-logo'>{letter}</span>
+            {[...'NUMÉRICA'].map((letter, index) => (
+              <span key={index} className='h-logo'>
+                {letter}
+              </span>
             ))}
           </h1>
         </header>
-        <Button variant='primary'>Ingresar</Button>
+        <Button variant='primary' onClick={handlerAuth}>
+          Ingresar
+        </Button>
       </main>
       <Footer />
     </div>
