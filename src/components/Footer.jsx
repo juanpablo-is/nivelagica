d8092d74645bb29dba398eb12c9d2bcf9d58a2a5
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-29 16:40:14 -0500

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      Desarrollado con 💜 por{' '}
+      <a href='https://www.linkedin.com/in/juanpablobarrerag/' target='_blank'>
+        jotape
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
