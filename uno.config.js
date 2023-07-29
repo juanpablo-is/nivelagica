51850f324aaaaf8d070dbc15a7f4add43391c7b6
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-29 16:40:39 -0500

    Add preset webfonts and create rules to homepage

--- uno.config.js
+++ uno.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetWebFonts, presetUno } from 'unocss'
 
 export default defineConfig({
   rules: [
@@ -7,11 +7,49 @@ export default defineConfig({
       ([_, num]) => ({
         'grid-template-columns': `repeat(auto-fit, minmax(${num}px, 1fr))`
       })
+    ],
+    [
+      /^fs-(.*)$/,
+      ([_, size]) => ({
+        'font-size': size.replace(/_/g, ' ')
+      })
+    ],
+    [
+      /^bg-image-\[(.*)\]$/,
+      ([_, bimage]) => ({
+        'background-image': bimage,
+        'background-size': '200% 100%'
+      })
     ]
   ],
   theme: {
     colors: {
       light: '#db005c'
+    },
+
+    animation: {
+      keyframes: {
+        'background-shine':
+          '{from { background-position: 0 0 } to { background-position: -200% 0 }}'
+      },
+      durations: {
+        'background-shine': '2s'
+      },
+      timingFns: {
+        'background-shine': 'linear'
+      },
+      counts: {
+        'background-shine': 'infinite'
+      }
     }
-  }
+  },
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        primary: 'Oswald'
+      }
+    })
+  ]
 })
