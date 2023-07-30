48f3f43bf8df49e35be0e8b806260a2cec4885ce
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-29 21:51:19 -0500

    Modify Twitch API to class

--- src/api/Twitch.js
+++ src/api/Twitch.js
@@ -1,26 +1,50 @@
 import { catchify } from '@/utils'
+class TwitchAPI {
+  static TWITCH_URL = 'https://id.twitch.tv/oauth2'
+  static HELIX_URL = 'https://api.twitch.tv/helix'
 
-const _URL = 'https://id.twitch.tv/oauth2'
+  #token = null
 
-const authURL = () => {
-  const url = new URL(`${_URL}/authorize`)
-  url.searchParams.set('client_id', 's7zgzmyk5osg4px90j6lfemmhukrx7')
-  url.searchParams.set('response_type', 'token')
-  url.searchParams.set('scope', 'openid moderator:manage:banned_users')
-  url.searchParams.set('redirect_uri', location.origin + '/oauth')
-  return url.href
+  constructor (token) {
+    this.#token = token
+  }
+
+  async getAccountInfo () {
+    return await catchify(
+      fetch(`${this.TWITCH_URL}/validate`, {
+        headers: { authorization: `OAuth ${this.#token}` }
+      }),
+      { json: true }
+    )
+  }
+
+  async timeoutUser ({ account, data }) {
+    const url = new URL(`${this.HELIX_URL}/moderation/bans`)
+    url.searchParams.set('broadcaster_id', account)
+    url.searchParams.set('moderator_id', account)
+
+    return await catchify(
+      fetch(url.href, {
+        method: 'POST',
+        headers: {
+          authorization: `Bearer ${this.#token}`,
+          'Client-Id': 's7zgzmyk5osg4px90j6lfemmhukrx7',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data })
+      }),
+      { json: true }
+    )
+  }
+
+  static authURL () {
+    const url = new URL(`${this.TWITCH_URL}/authorize`)
+    url.searchParams.set('client_id', 's7zgzmyk5osg4px90j6lfemmhukrx7')
+    url.searchParams.set('response_type', 'token')
+    url.searchParams.set('scope', 'openid moderator:manage:banned_users')
+    url.searchParams.set('redirect_uri', location.origin + '/oauth')
+    return url.href
+  }
 }
 
-const getAccountInfo = async token => {
-  return await catchify(
-    fetch(`${_URL}/validate`, {
-      headers: { authorization: `OAuth ${token}` }
-    }),
-    { json: true }
-  )
-}
-
-export default {
-  authURL,
-  getAccountInfo
-}
+export default TwitchAPI
