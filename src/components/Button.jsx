6e869b0f4230b507c0ef06786243ae158ed97343
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-04 12:12:31 -0500

    Add other props in Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,16 @@ const variants = {
     'inline-flex text-lg py-2 animate-background-shine items-center justify-center rounded-md border border-white bg-image-[linear-gradient(110deg,#000103,45%,#9146FF,55%,#000103)] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'
 }
 
-const Button = ({ children, className = '', variant = 'primary', onClick }) => {
+const Button = ({
+  children,
+  className = '',
+  variant = 'primary',
+  onClick,
+  ...props
+}) => {
   const _class = twMerge(variants[variant] ?? variants.primary, className)
   return (
-    <button className={_class} onClick={onClick}>
+    <button className={_class} onClick={onClick} {...props}>
       {children}
     </button>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import 'virtual:uno.css'
 import '@unocss/reset/tailwind-compat.css'
+import 'virtual:uno.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
