5d470faaf02c390df6437a6938cac9a67113ab2f
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-05 19:58:46 -0500

    Add timeout action in Twitch API

--- src/api/Twitch.js
+++ src/api/Twitch.js
@@ -1,25 +1,38 @@
 import { catchify } from '@/utils'
 class TwitchAPI {
-  static TWITCH_URL = 'https://id.twitch.tv/oauth2'
-  static HELIX_URL = 'https://api.twitch.tv/helix'
+  static #TWITCH_URL = 'https://id.twitch.tv/oauth2'
+  static #HELIX_URL = 'https://api.twitch.tv/helix'
 
   #token = null
 
-  constructor (token) {
+  constructor(token) {
     this.#token = token
   }
 
-  async getAccountInfo () {
+  async timeout({ user, time, game }) {
+    return await this.timeoutUser({
+      account: '96693344', // TODO: update
+      data: {
+        duration: time,
+        user_id: user,
+        reason: `Numérica en modo [${game}]`
+      },
+      token: this.#token
+    })
+  }
+
+  // API
+  async getAccountInfo() {
     return await catchify(
-      fetch(`${this.TWITCH_URL}/validate`, {
+      fetch(`${TwitchAPI.#TWITCH_URL}/validate`, {
         headers: { authorization: `OAuth ${this.#token}` }
       }),
       { json: true }
     )
   }
 
-  async timeoutUser ({ account, data }) {
-    const url = new URL(`${this.HELIX_URL}/moderation/bans`)
+  async timeoutUser({ account, data }) {
+    const url = new URL(`${TwitchAPI.#HELIX_URL}/moderation/bans`)
     url.searchParams.set('broadcaster_id', account)
     url.searchParams.set('moderator_id', account)
 
@@ -37,8 +50,8 @@ class TwitchAPI {
     )
   }
 
-  static authURL () {
-    const url = new URL(`${this.TWITCH_URL}/authorize`)
+  static authURL() {
+    const url = new URL(`${TwitchAPI.#TWITCH_URL}/authorize`)
     url.searchParams.set('client_id', 's7zgzmyk5osg4px90j6lfemmhukrx7')
     url.searchParams.set('response_type', 'token')
     url.searchParams.set('scope', 'openid moderator:manage:banned_users')
