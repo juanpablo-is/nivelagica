9e23a8ef5408222e64832e6ac354ebe963ea6a4b
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-15 11:34:39 -0500

    Add alert in command setting to prevent use bot commands

--- src/components/game/settings/Command.jsx
+++ src/components/game/settings/Command.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react'
+import Alert from '@/components/Alert'
+
+const NOT_ALLOW_COMMANDS = ['!game']
 
 const CommandSettings = () => {
   const [useCommand, setTimeout] = useState(false)
+
+  function handlerBlurInput (e) {
+    const value = e.target.value
+    if (NOT_ALLOW_COMMANDS.includes(value)) {
+      const confirm = window.confirm(
+        'Este comando posiblemente se esté usando en tu canal, ¿Desea usar igualmente este comando?'
+      )
+      if (confirm) {
+        const form = e.target.closest('form')
+        form.querySelector('button[type=submit]')?.click()
+      } else {
+        e.target.value = ''
+      }
+    }
+  }
+
   return (
     <div className='flex flex-col gap-2 w-full'>
       <div className='flex flex-col gap-1 border-2 rounded-md p-4'>
@@ -23,19 +42,26 @@ const CommandSettings = () => {
         </div>
 
         {useCommand && (
-          <div className='flex flex-col gap-2 w-full'>
-            <label htmlFor='command' className='text-sm font-medium'>
-              Ingrese comando
-            </label>
-            <input
-              id='command'
-              type='text'
-              name='command'
-              placeholder='Por ejemplo, !game'
-              required
-              className='text-black px-2 py-1 rounded-md'
-            />
-          </div>
+          <>
+            <Alert variant='warning'>
+              Ten cuidado en usar un comando de un bot de tu canal
+            </Alert>
+
+            <div className='flex flex-col gap-2 w-full'>
+              <label htmlFor='command' className='text-sm font-medium'>
+                Ingrese comando
+              </label>
+              <input
+                id='command'
+                type='text'
+                name='command'
+                placeholder='Por ejemplo, !game'
+                required
+                className='text-black px-2 py-1 rounded-md'
+                onBlur={handlerBlurInput}
+              />
+            </div>
+          </>
         )}
       </div>
     </div>
