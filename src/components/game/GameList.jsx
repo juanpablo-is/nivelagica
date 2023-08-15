6f3d49c80cbc9557e4e84f4468d817fd5ca960a3
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-14 22:17:15 -0500

    Dispatch drawer in GameList

--- src/components/game/GameList.jsx
+++ src/components/game/GameList.jsx
@@ -1,8 +1,23 @@
 import { useGames } from '@/store/index'
+import { addDrawer } from '@/Drawer/store'
 import Card from '../Card'
 
 const GameList = () => {
   const { games } = useGames()
+
+  function handlerShowDrawer ({ drawerComponent: Component, title }) {
+    if (Component) {
+      addDrawer(<Component />, {
+        position: 'bottom',
+        Header: (
+          <h5 className='inline-flex items-center gap-2 text-base font-semibold text-gray-400'>
+            <span className='i-lucide-info' /> {title}
+          </h5>
+        )
+      })
+    }
+  }
+
   return (
     <div className='flex flex-col gap-2 w-full'>
       <h2 className='text-lg'>Juegos:</h2>
@@ -18,6 +33,14 @@ const GameList = () => {
               className='peer absolute w-full h-full top-0 -z-1'
               required
             />
+
+            {game.drawerComponent && (
+              <span
+                className='i-lucide-help-circle absolute top-3 right-3 z-10 cursor-pointer hover:bg-accent'
+                onClick={() => handlerShowDrawer(game)}
+              />
+            )}
+
             <Card
               as='label'
               htmlFor={game.id}
