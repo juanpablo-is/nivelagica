d885f773ccfe06e792423fb59de3fdfa22b20af4
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-16 13:00:30 -0500

    Create super class to games mode

---
+++ src/games/index.js
@@ -0,0 +1,18 @@
+export class GameMode {
+  id;
+  title;
+  icon;
+  drawerComponent = null;
+
+  constructor(game) {
+    this.id = game.id
+    this.title = game.title
+    this.icon = game.icon
+  }
+
+  validate() { }
+
+  setDrawerComponent(component) {
+    this.drawerComponent = component
+  }
+}
