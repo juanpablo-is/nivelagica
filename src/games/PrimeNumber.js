5db0eaa765e9a787cc3d77978361c05eb9aa2e72
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-16 13:00:59 -0500

    Transform game TwitchNick and PrimeNumber to class GameMode

---
+++ src/games/PrimeNumber.js
@@ -0,0 +1,60 @@
+import { GameMode } from "./index";
+
+class PrimeNumber extends GameMode {
+  constructor() {
+    super({
+      id: 'prime',
+      title: 'Números primos',
+      icon: '123'
+    })
+  }
+
+  validate({ lastStore, data }) {
+    const { message: lastMessage, player: lastPlayer } = lastStore
+    const { message: message_data, message_type, display_name } = data
+    const [message, ...parts] = message_data.split(' ')
+
+    if (parts.length > 0) return { status: undefined }
+    if (message_type !== 'chat') return { status: undefined }
+    if (lastPlayer === display_name) return { status: undefined }
+
+    const number = Number(message)
+    if (Number.isNaN(number)) return { status: undefined }
+
+    if (lastMessage && number <= lastMessage) return { status: false }
+
+    const next_prime = this.nextPrime(lastMessage)
+    console.log({ next_prime });
+    return { status: next_prime === number }
+  }
+
+  isPrimeNumber(n) {
+    if (n <= 1) return false;
+
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) return false;
+    }
+
+    return true;
+  }
+
+  isPrime(num) {
+    let sqrtnum = Math.floor(Math.sqrt(num));
+    let prime = num !== 1;
+    for (let i = 2; i < sqrtnum + 1; i++) {
+      if (num % i === 0) {
+        prime = false;
+        break;
+      };
+    };
+    return prime;
+  }
+
+  nextPrime(num = 1) {
+    while (!this.isPrime(++num)) {
+    };
+    return num;
+  };
+}
+
+export default new PrimeNumber()
---
+++ src/games/TwitchNick.js
@@ -0,0 +1,33 @@
+import { GameMode } from "./index";
+import DrawerComponent from '@/components/drawers/TwitchNick'
+
+class TwitchNick extends GameMode {
+  constructor() {
+    super({
+      id: 'nick',
+      title: 'Twitch nick',
+      icon: '🥊'
+    })
+
+    this.setDrawerComponent(DrawerComponent)
+  }
+
+  validate({ lastStore, data }) {
+    const { message: lastMessage, player: lastPlayer } = lastStore
+    const { message: message_data, message_type, display_name } = data
+    const [message, ...parts] = message_data.split(' ')
+
+    if (parts.length > 0) return { status: undefined }
+    if (message_type !== 'chat') return { status: undefined }
+    if (lastPlayer === display_name) return { status: undefined }
+
+    if (!message.startsWith('@')) return { status: undefined }
+
+    if (!lastMessage) return { status: message === `@${display_name}` }
+    if (`@${lastPlayer}` !== message) return { status: false }
+
+    return { status: true }
+  }
+}
+
+export default new TwitchNick()
