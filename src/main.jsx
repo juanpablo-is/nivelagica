72e3ea80b065b34fc466a1c580555667e4620c45
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-16 19:50:54 -0500

    Install and add @vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+)
